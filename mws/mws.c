b836867a2a927744fe31adef81ee2e057f80d945
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-11 10:24:48 +0200

    Update mws.c

--- mws/mws.c
+++ mws/mws.c
@@ -7,6 +7,42 @@
 
 #define PORT 8000
 
+struct ethernet_headers {
+	// TODO
+};
+
+struct ip_headers {
+	// TODO
+};
+
+struct tcp_headers {
+	// TODO
+};
+
+void display_fatal_error_message() {
+	// TODO
+}
+
+void error_checked_malloc_wrapper() {
+	// TODO
+}
+
+void dump_row_memory() {
+	// TODO
+}
+
+int send_string() {
+	int outcome = 1;
+	// TODO
+	return outcome;
+}
+
+int receive_from_socket() {
+	int outcome = 0;
+	// TODO
+	return outcome;
+}
+
 int main(int argc, char **argv) {
 	int status = 0;
 	// TODO
