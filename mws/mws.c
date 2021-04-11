eff9e9e9a9061f36048904ffef06bb1948cfe42d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-11 09:53:55 +0200

    Create mws.c

---
+++ mws/mws.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8000
+
+int main(int argc, char **argv) {
+	int status = 0;
+	// TODO
+	return status;
+}
