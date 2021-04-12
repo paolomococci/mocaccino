37b4e0b22330ede1df7c1efa6caf21b3c961d150
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-12 10:24:48 +0200

    Update mws.c

--- mws/mws.c
+++ mws/mws.c
@@ -84,10 +84,10 @@ int get_filesize() {
 int main(int argc, char **argv) {
 
 	int status = 0;
-	int socket_file_descriptor0, socket_file_descriptor1;
+	int acceptance_socket_file_descriptor, connected_socket_file_descriptor;
 	int length_received = 1, yes = 1;
 	struct sockaddr_in host_address, client_address;
-	char buffer[1024];
+	char buffer[BUFFER_LENGTH];
 	socklen_t socket_length;
 
 	while(TRUE) {
