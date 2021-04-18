759fbc0f05d91705dbd0094353d50b6e30aedba9
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-18 16:13:49 +0200

    Update test_mariadb.c

--- test_mariadb/test_mariadb.c
+++ test_mariadb/test_mariadb.c
@@ -2,43 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <mysql/mysql.h>
-#include <mysql/my_global.h>
-#include <mysql/my_config.h>
-#include <mysql/my_byteorder.h>
 #include <mysql/mysqld_error.h>
+
+static const char *c_host = "127.0.0.1";
+static const char *c_username = "root";
+static const char *c_password = "password";
+static const char *c_port = "3306";
+static const char *c_dbname = "testdb";
 
 int main(void) {
 
-	MYSQL *connection_to_database = mysql_init(NULL);
-
-	if(connection_to_database == NULL) {
-		// TODO
-		exit(EXIT_FAILURE);
-	}
-
-	if(mysql_real_connect(
-			connection_to_database,
-			"localhost",
-			"root",
-			"password",
-			NULL,
-			0,
-			NULL,
-			0
-	)) {
-		// TODO
-		exit(EXIT_FAILURE);
-	}
-
-	if(mysql_query(
-			connection_to_database,
-			"CREATE DATABASE test_mariadb"
-	)) {
-		// TODO
-		exit(EXIT_FAILURE);
-	}
-
-	mysql_close(connection_to_database);
+	// TODO
 
 	exit(EXIT_SUCCESS);
 }
