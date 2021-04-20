e354830a6e064c943b22f4eb6fda8b1a8e0aeeac
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-20 09:12:33 +0200

    Update pws.c

--- pws/pws.c
+++ pws/pws.c
@@ -5,6 +5,16 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define FALSE 0
+#define TRUE !FALSE
+
+#define PORT 8000
+#define WEBROOT "./view"
+
+#define BUFFER_LENGTH 1024
+
+#define ETHERNET_ADDRESS_LENGTH 6
+
 int main(void) {
 	// TODO
 	return 0;
