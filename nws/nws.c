43eba5f00117a409ee7d8fac3726c1a2fff8f2b8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-20 09:17:07 +0200

    Update nws.c

--- nws/nws.c
+++ nws/nws.c
@@ -15,6 +15,13 @@
 
 #define ETHERNET_ADDRESS_LENGTH 6
 
+#define TCP_FIN   0x01
+#define TCP_SYN   0x02
+#define TCP_RST   0x04
+#define TCP_PUSH  0x08
+#define TCP_ACK   0x10
+#define TCP_URG   0x20
+
 int main(void) {
 	// TODO
 	return 0;
