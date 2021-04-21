9a41e687024aadfab793d8efa7e5953bd1905fc8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-21 11:35:10 +0200

    Update pws.c

--- pws/pws.c
+++ pws/pws.c
@@ -18,6 +18,9 @@
 #define END_OF_LINE "\r\n"
 #define END_OF_LINE_SIZE 2
 
+void handle_http_request(int, struct sockaddr_in *);
+void handle_fatal_error(char *);
+
 int main(void) {
 	// TODO
 	exit(EXIT_FAILURE);
