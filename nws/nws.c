7d97aab5dd7834b3bc973c41dfb9bba1f7080039
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-21 11:31:48 +0200

    Update nws.c

--- nws/nws.c
+++ nws/nws.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
 #include <netdb.h>
+#include <fcntl.h>
 
 #define FALSE 0
 #define TRUE !FALSE
 
 #define PORT 8000
-#define WEBROOT "./view"
+#define WEBROOT "./views"
+#define MAX_CONNECTIONS 10
 
 #define BUFFER_LENGTH 1024
 
