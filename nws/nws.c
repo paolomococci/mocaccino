78792b45fa7a6345cc04fafe6faefc962caecf6e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-21 11:33:23 +0200

    Update nws.c

--- nws/nws.c
+++ nws/nws.c
@@ -15,16 +15,8 @@
 #define WEBROOT "./views"
 #define MAX_CONNECTIONS 10
 
-#define BUFFER_LENGTH 1024
-
-#define ETHERNET_ADDRESS_LENGTH 6
-
-#define TCP_FIN   0x01
-#define TCP_SYN   0x02
-#define TCP_RST   0x04
-#define TCP_PUSH  0x08
-#define TCP_ACK   0x10
-#define TCP_URG   0x20
+#define END_OF_LINE "\r\n"
+#define END_OF_LINE_SIZE 2
 
 int main(void) {
 	// TODO
