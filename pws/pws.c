f2e3d90d1033701d8a4e8b9179127473d2f7a810
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-21 11:32:14 +0200

    Update pws.c

--- pws/pws.c
+++ pws/pws.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
 #include <fcntl.h>
 
@@ -12,7 +12,8 @@
 #define TRUE !FALSE
 
 #define PORT 8000
-#define WEBROOT "./view"
+#define WEBROOT "./views"
+#define MAX_CONNECTIONS 10
 
 #define BUFFER_LENGTH 1024
 
