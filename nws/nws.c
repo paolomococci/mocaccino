cd301c0cef2104b01c628bcf88677b7694a7f629
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-04-22 11:06:33 +0200

    Update nws.c

--- nws/nws.c
+++ nws/nws.c
@@ -18,6 +18,7 @@
 #define END_OF_LINE "\r\n"
 #define END_OF_LINE_SIZE 2
 
+void request_acceptance_message(int);
 void handle_http_request(int, struct sockaddr_in *);
 void handle_fatal_error(char *);
 
