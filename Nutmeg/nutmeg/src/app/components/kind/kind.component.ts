9d57d825876c07c5dc31ffe564a6d2ee0d572706
paolomococci <paolomococci@gmail.com>
2022-08-28 17:01:15 +0200

    new file:   Nutmeg/nutmeg/src/app/components/kind/kind.component.ts

---
+++ Nutmeg/nutmeg/src/app/components/kind/kind.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core'
+
+@Component({
+  selector: 'app-kind',
+  templateUrl: './kind.component.html',
+  styleUrls: ['./kind.component.sass']
+})
+export class KindComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
