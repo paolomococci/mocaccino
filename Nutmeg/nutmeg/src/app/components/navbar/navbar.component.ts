cae46f73b206074eff431f74b6f2627813433999
paolomococci <paolomococci@gmail.com>
2022-08-28 17:01:52 +0200

    new file:   Nutmeg/nutmeg/src/app/components/navbar/navbar.component.ts

---
+++ Nutmeg/nutmeg/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,47 @@
+import {
+  Component,
+  OnInit,
+  Input
+} from '@angular/core'
+
+import {
+  BreakpointObserver,
+  Breakpoints
+} from '@angular/cdk/layout'
+
+import {
+  Observable
+} from 'rxjs'
+
+import {
+  map,
+  shareReplay
+ } from 'rxjs/operators'
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.sass']
+})
+export class NavbarComponent implements OnInit {
+
+  @Input()
+  title!: string
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(
+      Breakpoints.Handset
+    )
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    )
+
+  constructor(
+    private breakpointObserver: BreakpointObserver
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
