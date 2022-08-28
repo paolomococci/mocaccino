f785b09b2a672d77935fa20f3fb1a2e91e4e13f6
paolomococci <paolomococci@gmail.com>
2022-08-28 20:05:47 +0200

    modified:   Nutmeg/nutmeg/src/app/services/kind.service.ts

--- Nutmeg/nutmeg/src/app/services/kind.service.ts
+++ Nutmeg/nutmeg/src/app/services/kind.service.ts
@@ -1,5 +1,21 @@
 import { Injectable } from '@angular/core'
 
+import {
+  Observable,
+  of
+} from 'rxjs'
+
+import {
+  catchError,
+  map,
+  tap
+} from 'rxjs/operators'
+
+import {
+  HttpClient,
+  HttpHeaders
+} from '@angular/common/http'
+
 @Injectable({
   providedIn: 'root'
 })
