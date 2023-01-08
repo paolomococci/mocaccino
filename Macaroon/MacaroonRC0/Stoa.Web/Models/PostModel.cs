8a9ec8dbc8dcf9a2169f206eadcd1f03fc2f8280
paolomococci <paolomococci@gmail.com>
2023-01-08 12:30:11 +0100

    new file:   Macaroon/MacaroonRC0/Stoa.Web/Models/PostModel.cs

---
+++ Macaroon/MacaroonRC0/Stoa.Web/Models/PostModel.cs
@@ -0,0 +1,7 @@
+namespace Stoa.Web.Models;
+
+public class PostModel {
+  public string Caption { get; set; } = string.Empty;
+  public string Description { get; set; } = string.Empty;
+  public IFormFile? Concept { get; set; }
+}
