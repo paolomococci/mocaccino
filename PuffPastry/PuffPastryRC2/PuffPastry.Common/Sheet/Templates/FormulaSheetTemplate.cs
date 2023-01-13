48ffa01786037d65b3e8242b2b3b25330a262295
paolomococci <paolomococci@gmail.com>
2023-01-13 17:17:39 +0100

    new file:   PuffPastry/PuffPastryRC2/PuffPastry.Common/Sheet/Templates/FormulaSheetTemplate.cs

---
+++ PuffPastry/PuffPastryRC2/PuffPastry.Common/Sheet/Templates/FormulaSheetTemplate.cs
@@ -0,0 +1,11 @@
+using ClosedXML.Excel;
+
+namespace PuffPastry.Common.Sheet.Templates;
+
+public class FormulaSheetTemplate
+{
+  internal static void Transcribe(XLWorkbook ledger)
+  {
+    throw new NotImplementedException();
+  }
+}
