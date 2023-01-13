a7cad20df4fc3d475101914fa9f13034d7c64dba
paolomococci <paolomococci@gmail.com>
2023-01-13 16:08:59 +0100

    modified:   PuffPastry/PuffPastryRC1/PuffPastry.Common/Sheet/Templates/PinnedSheetTemplate.cs

--- PuffPastry/PuffPastryRC1/PuffPastry.Common/Sheet/Templates/PinnedSheetTemplate.cs
+++ PuffPastry/PuffPastryRC1/PuffPastry.Common/Sheet/Templates/PinnedSheetTemplate.cs
@@ -1,3 +1,11 @@
+using ClosedXML.Excel;
+
 namespace PuffPastry.Common.Sheet.Templates;
 
-public class PinnedSheetTemplate {}
+public class PinnedSheetTemplate
+{
+  internal static void Transcribe(XLWorkbook ledger)
+  {
+    throw new NotImplementedException();
+  }
+}
