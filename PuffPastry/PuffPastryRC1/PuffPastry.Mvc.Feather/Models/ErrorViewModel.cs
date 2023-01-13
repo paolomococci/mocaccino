54262b609eb1682ebe11f3a4578434f3c4cd9247
paolomococci <paolomococci@gmail.com>
2023-01-13 16:29:49 +0100

    modified:   PuffPastry/PuffPastryRC1/PuffPastry.Mvc.Feather/Models/ErrorViewModel.cs

--- PuffPastry/PuffPastryRC1/PuffPastry.Mvc.Feather/Models/ErrorViewModel.cs
+++ PuffPastry/PuffPastryRC1/PuffPastry.Mvc.Feather/Models/ErrorViewModel.cs
@@ -1,3 +1,7 @@
 namespace PuffPastry.Mvc.Feather.Models;
 
-public class ErrorViewModel {}
+public class ErrorViewModel {
+  public string? RequestId { get; set; }
+
+  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
