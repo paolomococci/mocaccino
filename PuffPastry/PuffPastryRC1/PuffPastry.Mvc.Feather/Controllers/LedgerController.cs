db59fd00e8f726eef52149b75727fd93df1acd2c
paolomococci <paolomococci@gmail.com>
2023-01-13 16:54:53 +0100

    modified:   PuffPastry/PuffPastryRC1/PuffPastry.Mvc.Feather/Controllers/LedgerController.cs

--- PuffPastry/PuffPastryRC1/PuffPastry.Mvc.Feather/Controllers/LedgerController.cs
+++ PuffPastry/PuffPastryRC1/PuffPastry.Mvc.Feather/Controllers/LedgerController.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace PuffPastry.Mvc.Feather.Controllers;
 
-public class LedgerController : Controller {
+public class LedgerController : Controller
+{
   private readonly ILogger<LedgerController> _logger;
 
   public LedgerController(
     ILogger<LedgerController> logger
-  ) {
+  )
+  {
     this._logger = logger;
   }
 
