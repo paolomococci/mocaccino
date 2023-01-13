e1717bf5636cbdac205d4e5844c79ae48719c1e3
paolomococci <paolomococci@gmail.com>
2023-01-13 16:04:38 +0100

    modified:   PuffPastry/PuffPastryRC1/PuffPastry.Common/Models/ItemModel.cs

--- PuffPastry/PuffPastryRC1/PuffPastry.Common/Models/ItemModel.cs
+++ PuffPastry/PuffPastryRC1/PuffPastry.Common/Models/ItemModel.cs
@@ -1,3 +1,50 @@
 namespace PuffPastry.Common.Models;
 
-public class ItemModel {}
+public class ItemModel {
+  public string Code { get; set; } = string.Empty;
+  public int Orders { get; set; }
+  public bool Delivered { get; set; } = false;
+  public DateTime Period { get; set; }
+
+  public ItemModel() { }
+
+  public ItemModel(
+    string Code
+  )
+  {
+    this.Code = Code;
+  }
+
+  public ItemModel(
+    string Code,
+    int Orders
+  )
+  {
+    this.Code = Code;
+    this.Orders = Orders;
+  }
+
+  public ItemModel(
+    string Code,
+    int Orders,
+    bool Delivered
+  )
+  {
+    this.Code = Code;
+    this.Orders = Orders;
+    this.Delivered = Delivered;
+  }
+
+  public ItemModel(
+    string Code,
+    int Orders,
+    bool Delivered,
+    DateTime Period
+  )
+  {
+    this.Code = Code;
+    this.Orders = Orders;
+    this.Delivered = Delivered;
+    this.Period = Period;
+  }
+}
