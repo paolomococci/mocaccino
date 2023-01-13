71756acce91c3fa73d7ceaf6081d9306f1f75dd3
paolomococci <paolomococci@gmail.com>
2023-01-13 19:25:10 +0100

    modified:   PuffPastry/PuffPastryRC2/PuffPastry.Common/Sheet/Templates/PinnedSheetTemplate.cs

--- PuffPastry/PuffPastryRC2/PuffPastry.Common/Sheet/Templates/PinnedSheetTemplate.cs
+++ PuffPastry/PuffPastryRC2/PuffPastry.Common/Sheet/Templates/PinnedSheetTemplate.cs
@@ -1,4 +1,5 @@
 using ClosedXML.Excel;
+using PuffPastry.Common.Models;
 
 namespace PuffPastry.Common.Sheet.Templates;
 
@@ -6,6 +7,97 @@ public class PinnedSheetTemplate
 {
   internal static void Transcribe(XLWorkbook ledger)
   {
+    List<ItemModel> items = new();
+    items.Add(new ItemModel(
+      "10111111",
+      150,
+      true,
+      new DateTime(2010, 1, 1)
+    ));
+    items.Add(new ItemModel(
+      "10111121",
+      230,
+      false,
+      new DateTime(2010, 1, 1)
+    ));
+    items.Add(new ItemModel(
+      "10111131",
+      510,
+      true,
+      new DateTime(2010, 1, 1)
+    ));
+    items.Add(new ItemModel(
+      "10111321",
+      240,
+      true,
+      new DateTime(2010, 1, 1)
+    ));
+    items.Add(new ItemModel(
+      "10145111",
+      70,
+      false,
+      new DateTime(2010, 1, 1)
+    ));
+    items.Add(new ItemModel(
+      "10111111",
+      650,
+      true,
+      new DateTime(2010, 2, 1)
+    ));
+    items.Add(new ItemModel(
+      "10111121",
+      105,
+      true,
+      new DateTime(2010, 2, 1)
+    ));
+    items.Add(new ItemModel(
+      "10111131",
+      206,
+      false,
+      new DateTime(2010, 2, 1)
+    ));
+    items.Add(new ItemModel(
+      "10111321",
+      451,
+      true,
+      new DateTime(2010, 2, 1)
+    ));
+    items.Add(new ItemModel(
+      "10145111",
+      305,
+      false,
+      new DateTime(2010, 2, 1)
+    ));
+    items.Add(new ItemModel(
+      "10111111",
+      560,
+      true,
+      new DateTime(2010, 3, 1)
+    ));
+    items.Add(new ItemModel(
+      "10111121",
+      307,
+      true,
+      new DateTime(2010, 3, 1)
+    ));
+    items.Add(new ItemModel(
+      "10111131",
+      412,
+      false,
+      new DateTime(2010, 3, 1)
+    ));
+    items.Add(new ItemModel(
+      "10111321",
+      230,
+      true,
+      new DateTime(2010, 3, 1)
+    ));
+    items.Add(new ItemModel(
+      "10145111",
+      505,
+      false,
+      new DateTime(2010, 3, 1)
+    ));
     throw new NotImplementedException();
   }
 }
