6351c52ef56f0692fb4554e93c094192560cf5ac
paolomococci <paolomococci@gmail.com>
2023-01-14 08:24:56 +0100

    new file:   PuffPastry/PuffPastryRC4/PuffPastry.Common/Models/LedgerModel.cs

---
+++ PuffPastry/PuffPastryRC4/PuffPastry.Common/Models/LedgerModel.cs
@@ -0,0 +1,38 @@
+using ClosedXML.Excel;
+using PuffPastry.Common.Sheet.Templates;
+
+namespace PuffPastry.Common.Models;
+
+public class LedgerModel
+{
+  public static byte[] Perform(MemoryStream memoryStream)
+  {
+
+    /* generate the workbook */
+    XLWorkbook ledger = new XLWorkbook();
+
+    /* adds three spreadsheets */
+    List<string> sheetNames = new List<string>() {
+        "FormulaSheet",
+        "DatatypeSheet",
+        "PinnedSheet"
+      };
+    foreach (var sheetName in sheetNames)
+    {
+      var sheet = ledger.Worksheets.Add(sheetName);
+      sheet.Cell("A1").Value = sheetName;
+    }
+
+    FormulaSheetTemplate.Transcribe(ledger);
+    DatatypeSheetTemplate.Transcribe(ledger);
+    PinnedSheetTemplate.Transcribe(ledger);
+
+    /* pack it all up */
+    ledger.SaveAs(memoryStream);
+    memoryStream.Seek(
+      0,
+      SeekOrigin.Begin
+    );
+    return memoryStream.ToArray();
+  }
+}
