d6bb4923fd1b49c4b36a91c130c137b122299cb4
paolomococci <paolomococci@gmail.com>
2023-01-14 06:57:54 +0100

    modified:   PuffPastry/PuffPastryRC3/PuffPastry.Common/Sheet/Templates/FormulaSheetTemplate.cs

--- PuffPastry/PuffPastryRC3/PuffPastry.Common/Sheet/Templates/FormulaSheetTemplate.cs
+++ PuffPastry/PuffPastryRC3/PuffPastry.Common/Sheet/Templates/FormulaSheetTemplate.cs
@@ -8,13 +8,13 @@ public class FormulaSheetTemplate
   {
     /* adds data and formulas to the spreadsheet named FormulaSheet */
     var formulaSheet = ledger.Worksheet("FormulaSheet");
-    formulaSheet.Cell("B1").SetValue("Values");
-    formulaSheet.Cell("B2").SetValue(4.55);
-    formulaSheet.Cell("B3").SetValue(2.55);
-    formulaSheet.Cell("B4").SetValue(4.2);
-    formulaSheet.Cell("B5").SetValue(3.33);
-    formulaSheet.Cell("A6").SetValue("Sum");
-    formulaSheet.Cell("A7").SetValue("Average");
+    formulaSheet.Cell("B1").Value = "Values";
+    formulaSheet.Cell("B2").Value = 4.55;
+    formulaSheet.Cell("B3").Value = 2.55;
+    formulaSheet.Cell("B4").Value = 4.2;
+    formulaSheet.Cell("B5").Value = 3.33;
+    formulaSheet.Cell("A6").Value = "Sum";
+    formulaSheet.Cell("A7").Value = "Average";
     formulaSheet.Cell("B6").FormulaA1 = "SUM(B2:B5)";
     formulaSheet.Cell("B7").FormulaA1 = "AVERAGE(B2:B5)";
     /* performs calculations programmatically */
@@ -23,12 +23,8 @@ public class FormulaSheetTemplate
     decimalNumbers.Add(formulaSheet.Cell("B3").GetValue<decimal>());
     decimalNumbers.Add(formulaSheet.Cell("B4").GetValue<decimal>());
     decimalNumbers.Add(formulaSheet.Cell("B5").GetValue<decimal>());
-    formulaSheet.Cell("C1").SetValue("compute programmatically");
-    formulaSheet.Cell("C6").SetValue(
-      decimalNumbers.Sum()
-    );
-    formulaSheet.Cell("C7").SetValue(
-      decimalNumbers.Sum() / decimalNumbers.Count()
-    );
+    formulaSheet.Cell("C1").Value = "compute programmatically";
+    formulaSheet.Cell("C6").Value = decimalNumbers.Sum();
+    formulaSheet.Cell("C7").Value = decimalNumbers.Sum() / decimalNumbers.Count();
   }
 }
