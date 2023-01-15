db98e3368ae7c957934f3a2c2183eab8cff6f5c9
paolomococci <paolomococci@gmail.com>
2023-01-15 02:27:33 +0100

    deleted:    PuffPastry/PuffPastryRC4/PuffPastry.Mvc.Feather/Models/ErrorViewModel.cs

--- PuffPastry/PuffPastryRC4/PuffPastry.Mvc.Feather/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace PuffPastry.Mvc.Feather.Models;
-
-public class ErrorViewModel {
-  public string? RequestId { get; set; }
-
-  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
