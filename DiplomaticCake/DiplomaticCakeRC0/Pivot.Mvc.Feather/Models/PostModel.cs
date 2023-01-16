65e34672fb44c1a7abee4e58d32f9cacf7245000
paolomococci <paolomococci@gmail.com>
2023-01-16 16:25:44 +0100

    new file:   DiplomaticCake/DiplomaticCakeRC0/Pivot.Mvc.Feather/Models/PostModel.cs

---
+++ DiplomaticCake/DiplomaticCakeRC0/Pivot.Mvc.Feather/Models/PostModel.cs
@@ -0,0 +1,8 @@
+namespace Pivot.Mvc.Feather.Models;
+
+public class PostModel
+{
+  public string Caption { get; set; } = string.Empty;
+  public string Description { get; set; } = string.Empty;
+  public IFormFile? Concept { get; set; }
+}
