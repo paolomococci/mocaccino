13cb9179d6e2ae66f636456a47b3d188ca34fc03
paolomococci <paolomococci@gmail.com>
2023-02-03 08:58:19 +0100

    new file:   RedVelvet/Estimate.RC0/Estimate.Common/Models/ReportModel.cs

---
+++ RedVelvet/Estimate.RC0/Estimate.Common/Models/ReportModel.cs
@@ -0,0 +1,9 @@
+namespace Estimate.Common.Models;
+
+[Serializable]
+public class ReportModel
+{
+  public string Name { get; set; } = string.Empty;
+  public string Area { get; set; } = string.Empty;
+  public string Price { get; set; } = string.Empty;
+}
