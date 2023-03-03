2e6721b6bc8f22b551a16e0379d594e7a684aa49
paolomococci <paolomococci@gmail.com>
2023-03-03 18:55:05 +0100

    modified:   Classic/delphic.rc0/app/Http/Controllers/Auth/RegisteredUserController.php

--- Classic/delphic.rc0/app/Http/Controllers/Auth/RegisteredUserController.php
+++ Classic/delphic.rc0/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -10,7 +10,6 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -33,7 +32,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:'.User::class,
+            'email' => 'required|string|email|max:255|unique:' . User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
