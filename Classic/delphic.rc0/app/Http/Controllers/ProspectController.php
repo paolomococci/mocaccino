9b3b8323137f1911730bc1e5a3dcfdf599668bb1
paolomococci <paolomococci@gmail.com>
2023-03-03 22:08:57 +0100

    modified:   Classic/delphic.rc0/app/Http/Controllers/ProspectController.php

--- Classic/delphic.rc0/app/Http/Controllers/ProspectController.php
+++ Classic/delphic.rc0/app/Http/Controllers/ProspectController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Prospect;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class ProspectController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        return Inertia::render(
+            'Prospects/Index',
+            [
+                'prospects' => Prospect::with('user:id,name')->latest()->get(),
+            ]
+        );
     }
 
     /**
@@ -26,9 +34,16 @@ class ProspectController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate(
+            [
+                'message' => 'required|string|max:255',
+            ]
+        );
+        $request->user()->prospects()->create($validated);
+
+        return redirect(route('prospects.index'));
     }
 
     /**
@@ -50,16 +65,31 @@ class ProspectController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Prospect $prospect)
-    {
-        //
+    public function update(
+        Request $request,
+        Prospect $prospect
+    ): RedirectResponse {
+        $this->authorize('update', $prospect);
+        $validated = $request->validate(
+            [
+                'message' => 'required|string|max:255',
+            ]
+        );
+        $prospect->update($validated);
+
+        return redirect(
+            route('prospects.index')
+        );
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Prospect $prospect)
+    public function destroy(Prospect $prospect): RedirectResponse
     {
-        //
+        $this->authorize('delete', $prospect);
+        $prospect->delete();
+
+        return redirect(route('prospects.index'));
     }
 }
