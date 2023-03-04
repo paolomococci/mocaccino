f626fd2d0cba97b9fd2c1c24791b50e103d01639
paolomococci <paolomococci@gmail.com>
2023-03-04 19:36:57 +0100

    modified:   Classic/delphic.rc0/resources/js/Pages/Prospects/Index.jsx

--- Classic/delphic.rc0/resources/js/Pages/Prospects/Index.jsx
+++ Classic/delphic.rc0/resources/js/Pages/Prospects/Index.jsx
@@ -24,7 +24,7 @@ export default function Index({ auth, prospects }) {
 
             <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                 <form onSubmit={submit}>
-                    <textarea value={data.message} placeholder="What do you want to complain about today?"
+                    <textarea value={data.message} placeholder="State your point of view."
                         className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                         onChange={e => setData('message', e.target.value)}>
                     </textarea>
