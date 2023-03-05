54c072b9aa4b5e586a9c0453232cf1f1c2beae41
paolomococci <paolomococci@gmail.com>
2023-03-05 16:58:19 +0100

    modified:   Milkshake/milkshake.rc0/milkshake-api/src/index.ts

--- Milkshake/milkshake.rc0/milkshake-api/src/index.ts
+++ Milkshake/milkshake.rc0/milkshake-api/src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv'
 import config from '../config/config.json'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
 
 dotenv.config()
 
