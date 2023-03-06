6f58875ef75c2dd19578e3fa3812f337e161b153
paolomococci <paolomococci@gmail.com>
2023-03-06 15:45:29 +0100

    modified:   Milkshake/milkshake.rc0/milkshake-api/src/lib/auth.ts

--- Milkshake/milkshake.rc0/milkshake-api/src/lib/auth.ts
+++ Milkshake/milkshake.rc0/milkshake-api/src/lib/auth.ts
@@ -1,10 +1,23 @@
 import {
-    encrypt, 
+    encrypt,
     isPasswordMatch
 } from '@contentpi/lib'
 import {
-    IUser, 
-    IModels, 
+    IUser,
+    IModels,
     IAuthPayload
 } from '../types'
 import { createToken } from './jwt'
+
+export const getUserBy = async (
+    where: any,
+    models: IModels
+): Promise<IUser> => {
+    const user = await models.User.findOne(
+        {
+            where,
+            raw: true
+        }
+    )
+    return user
+}
