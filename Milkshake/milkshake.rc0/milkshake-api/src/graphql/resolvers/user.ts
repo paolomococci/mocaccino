ecbd960883ea62d6b39a4ba299ac520fedd2a7bd
paolomococci <paolomococci@gmail.com>
2023-03-06 17:21:04 +0100

    modified:   Milkshake/milkshake.rc0/milkshake-api/src/graphql/resolvers/user.ts

--- Milkshake/milkshake.rc0/milkshake-api/src/graphql/resolvers/user.ts
+++ Milkshake/milkshake.rc0/milkshake-api/src/graphql/resolvers/user.ts
@@ -12,6 +12,43 @@ import {
 } from '../../lib/auth'
 
 export default {
-    Query: {},
+    Query: {
+        getUsers: (
+            _: any,
+            args: any,
+            { models }: { models: IModels }
+        ): IUser[] => models.User.findAll(),
+        getUserData: async (
+            _: any,
+            { at }: { at: string },
+            { models }: { models: IModels }
+        ): Promise<any> => {
+            const connectedUser = await getUserData(at)
+            if (connectedUser) {
+                const user = await getUserBy(
+                    {
+                        id: connectedUser.id,
+                        email: connectedUser.email,
+                        privilege: connectedUser.privilege,
+                        active: connectedUser.active
+                    },
+                    models
+                )
+                if (user) {
+                    return {
+                        ...connectedUser
+                    }
+                }
+            }
+            return {
+                id: '',
+                username: '',
+                password: '',
+                email: '',
+                privilege: '',
+                active: false
+            }
+        }
+    },
     Mutation: {}
 }
