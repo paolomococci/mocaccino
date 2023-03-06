f45fbca90d4f5fb0e991beaab5bb846262c0b701
paolomococci <paolomococci@gmail.com>
2023-03-06 15:57:47 +0100

    modified:   Milkshake/milkshake.rc0/milkshake-api/src/lib/auth.ts

--- Milkshake/milkshake.rc0/milkshake-api/src/lib/auth.ts
+++ Milkshake/milkshake.rc0/milkshake-api/src/lib/auth.ts
@@ -21,3 +21,32 @@ export const getUserBy = async (
     )
     return user
 }
+
+export const doLogin = async (
+    email: string,
+    password: string,
+    models: IModels
+): Promise<IAuthPayload> => {
+    const user = await getUserBy(
+        { email },
+        models
+    )
+    if (!user) {
+        // TODO, invalid login: the user is not registered
+    }
+    const passwordMatch = isPasswordMatch(
+        encrypt(password),
+        user.password
+    )
+    const isActive = user.active
+    if (!passwordMatch) {
+        // TODO, invalid login: the password is not correct
+    }
+    if (!isActive) {
+        // TODO, invalid login: the user is not active 
+    }
+    const [token] = await createToken(user)
+    return {
+        token
+    }
+}
