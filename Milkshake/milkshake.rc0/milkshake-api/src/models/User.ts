7a878747be38925f5acc1bf602ed615afc1f712e
paolomococci <paolomococci@gmail.com>
2023-03-06 04:54:49 +0100

    modified:   Milkshake/milkshake.rc0/milkshake-api/src/models/User.ts

--- Milkshake/milkshake.rc0/milkshake-api/src/models/User.ts
+++ Milkshake/milkshake.rc0/milkshake-api/src/models/User.ts
@@ -7,8 +7,17 @@ import {
 export default (sequelize: any, DataTypes: IDataTypes): IUser => {
     const User = sequelize.define(
         'User',
-        {},
-        {}
+        {
+            id: {},
+            username: {},
+            password: {},
+            email: {},
+            privilege: {},
+            active: {}
+        },
+        {
+            hooks: {}
+        }
     )
     return User
 }
