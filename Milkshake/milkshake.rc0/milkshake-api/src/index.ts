cd9002fac1ca4f522c7b96713084b604f97f137d
paolomococci <paolomococci@gmail.com>
2023-03-07 12:54:37 +0100

    modified:   Milkshake/milkshake.rc0/milkshake-api/src/index.ts

--- Milkshake/milkshake.rc0/milkshake-api/src/index.ts
+++ Milkshake/milkshake.rc0/milkshake-api/src/index.ts
@@ -1,2 +1,14 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
+
+const typeDefs = `#graphql
+    type Movie {
+        title: String
+        director: String
+        genre: String
+    }
+
+    type Query {
+        movies: [Movie]
+    }
+`
