850c33405f103895c68348e1ccbe371ebd0d8644
paolomococci <paolomococci@gmail.com>
2023-03-07 07:00:46 +0100

    deleted:    Milkshake/milkshake.rc0/milkshake-api/src/index.ts

--- Milkshake/milkshake.rc0/milkshake-api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloServer } from '@apollo/server'
-import { makeExecutableSchema } from '@graphql-tools/schema'
-import models from './models'
-import { $server } from '../config'
-
-const alter = true
-const force = false
