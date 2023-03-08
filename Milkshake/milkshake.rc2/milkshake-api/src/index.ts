89e6300a21e5de9ca75fb5d76ca433c2b226b462
paolomococci <paolomococci@gmail.com>
2023-03-08 09:23:56 +0100

    modified:   Milkshake/milkshake.rc2/milkshake-api/src/index.ts

--- Milkshake/milkshake.rc2/milkshake-api/src/index.ts
+++ Milkshake/milkshake.rc2/milkshake-api/src/index.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import models from './models'
 import { $server } from '../config'
