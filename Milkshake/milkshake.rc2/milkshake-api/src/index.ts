084c788892aed2c1dec6aaf0fd6ed5608f7b1928
paolomococci <paolomococci@gmail.com>
2023-03-08 16:36:50 +0100

    modified:   Milkshake/milkshake.rc2/milkshake-api/src/index.ts

--- Milkshake/milkshake.rc2/milkshake-api/src/index.ts
+++ Milkshake/milkshake.rc2/milkshake-api/src/index.ts
@@ -1,8 +1,11 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import { makeExecutableSchema } from '@graphql-tools/schema'
-import models from './models'
+
 import { $server } from '../config'
+import models from './models'
+import resolvers from './graphql/resolvers'
+import typeDefs from './graphql/types'
 
 const alter = true
 const force = false
