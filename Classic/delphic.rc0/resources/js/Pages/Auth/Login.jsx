bc4e7d5c1c01d39cd403ada5e38581dd630433ec
paolomococci <paolomococci@gmail.com>
2023-03-14 11:04:02 +0100

    modified:   Classic/delphic.rc0/resources/js/Pages/Auth/Login.jsx

--- Classic/delphic.rc0/resources/js/Pages/Auth/Login.jsx
+++ Classic/delphic.rc0/resources/js/Pages/Auth/Login.jsx
@@ -78,13 +78,13 @@ export default function Login({ status, canResetPassword }) {
                 </div>
 
                 <div className="flex items-center justify-end mt-4">
-                        <Link
-                            href={route('register')}
-                            className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                        >
-                            Do you want to register?
-                        </Link>
-                        &#x2006;
+                    <Link
+                        href={route('register')}
+                        className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    >
+                        Do you want to register?
+                    </Link>
+                    &#x2006;
                     {canResetPassword && (
                         <Link
                             href={route('password.request')}
